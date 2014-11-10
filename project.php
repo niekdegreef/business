dcfc8d9e4dc9abd25c5cb56f085e8204acf106e0
Niek de Greef <niek@beyerdegreef.com>
2014-11-10 15:54:13 +0200

    convert static pages to wp-theme pages

--- project.php
+++ project.php
@@ -1,59 +1,49 @@
-<?php include 'header.php' ?>
-    
-
-    <div id="content" class="site-content group">
-
-    <!-- end header -->
+<?php
+/**
+* template name: Project listing 
+* @package business
+*/
+get_header(); ?> 
 
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-        <article class="projects">
-            <header>
-                <h1>What we do - products &amp; services</h1>
-            </header>
+        <?php if ( have_posts() ) : ?>
 
-            <section>
-                <h2>Machine building &amp; architecture</h2>
-                <figure>
-                    <img src="images/products.jpg" alt="">
-                </figure>
-                <p>
-                BDG specialises in Machine building &amp; architecture. This text is only here for the purpose of showing you a full paragraph. With a lot of practical knowledge and a fresh perspective, we look at your product. Together we work out your ideas without having your own engineering department before being charged extra. In preliminary studies, detailed drawings, modifications to existing equipment or structures; In all these cases, you can appeal to the designers of our company. We create technically correct designs &amp; animations of working machines.
-                </p>
-            </section>
+            <?php /* Start the Loop for first column take content from home-page, ACF repeater field*/ ?>
+                <article class="projects">
+                
+                    <header>
+                        <h1><?php the_title() ?></h1>
+                    </header>
 
-            <section>
-                <h2>Technical installations &amp; designs</h2>
-                <figure>
-                    <img src="images/products1.jpg" alt="">
-                </figure>
-                <p>
-                BDG specialises in Technical installations &amp; designs. This text is only here for the purpose of showing you a full paragraph. With a lot of practical knowledge and a fresh perspective, we look at your product. In preliminary studies, detailed drawings, modifications to existing equipment or structures; In all these cases, you can appeal to the designers of our company. We create technically correct designs &amp; animations of working machines.
-                </p>
-            </section>
+                    <?php while ( have_posts() ) : the_post(); ?>
 
-            <section>
-                <h2>Electrical installations</h2>
-                <figure>
-                    <img src="images/products2.jpg" alt="">
-                </figure>
-                <p>
-                BDG specialises in Electrical installations. This text is only here for the purpose of showing you a full paragraph. With a lot of practical knowledge and a fresh perspective, we look at your product. In preliminary studies, detailed drawings, modifications to existing equipment or structures; In all these cases, you can appeal to the designers of our company. We create technically correct designs &amp; animations of working machines.
-                </p>
-            </section>
-        </article>
+                        <section>
+                            <h2><?php get_field('name') ?></h2>
+                            <figure><img src="<?php get_field('project') ?>" alt=""> </figure>
+                            <p><?php get_field('project_description') ?></p>
+                        </section>
 
-        <a class="contact" href="#">
-            <p>contact us for more information about available personel &amp; software packages</p>
-            <span class="genericon genericon-mail"></span>
-        </a>
+                    <?php endwhile; ?>
+
+                </article>
+
+                <!-- optional extra contact button, if page is long -->
+               <!--  <a class="contact" href="<?php //get_field('link') ?>">
+                    <p><?php //get_field('button_text') ?></p>
+                    <span class="genericon genericon-mail"></span>
+                </a> -->
+
+        
+        <?php endif; ?> 
         
         </main><!-- #main -->
 
     </div><!-- #primary -->
 
-   <?php include 'sidebar.php' ?>
+    <?php get_sidebar(); ?>
 
     </div><!-- #content -->
-<?php include 'footer.php' ?>
+
+<?php get_footer(); ?>
