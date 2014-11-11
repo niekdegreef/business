fc11f6e553716f958862f5c3929f6fb4c874c5c0
Niek de Greef <niek@beyerdegreef.com>
2014-11-11 10:08:25 +0200

    add wp files, put them in place

--- index.php
+++ index.php
@@ -1,39 +1,17 @@
 <?php
 /**
- * The main template file. @package business
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
  */
-get_header(); ?> 
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-        <?php if ( have_posts() ) : ?>
-
-            <?php /* Start the Loop */ ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-
-                <?php
-                    /* Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    get_template_part( 'content', get_post_format() );
-                ?>
-
-            <?php endwhile; ?>
-
-            <?php business_paging_nav(); ?>
-
-        <?php else : ?>
-
-            <?php get_template_part( 'content', 'none' ); ?>
-
-        <?php endif; ?>
-
-        </main><!-- #main -->
-    </div><!-- #primary -->
-
-    <?php get_sidebar(); ?>
-
-    </div><!-- #content -->
-<?php get_footer(); ?>
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
