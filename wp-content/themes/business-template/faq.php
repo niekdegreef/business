4828eda50ccbdb2cb012c71760212edefe245b62
Niek de Greef <niek@beyerdegreef.com>
2014-11-25 17:07:16 +0200

    working on faq template

---
+++ wp-content/themes/business-template/faq.php
@@ -0,0 +1,84 @@
+<?php
+/**
+* template name: FAQ page with accordion dropdown
+* @package business
+*/
+get_header(); ?> 
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+        <?php
+        $frequently_asked_question = get_field('frequently_asked_question');
+            
+            if( $frequently_asked_question ): ?>
+
+                <header>
+                    <h1><?php the_title() ?></h1>
+                </header>
+
+                <dl class="accordion">
+                    <?php while( has_sub_field('frequently_asked_question') ): 
+                       
+                        $question = get_sub_field('question');
+                        $answer = get_sub_field('answer'); ?>
+
+                            <dt><a href=""><?php echo $question ?> </a></dt>
+                            <dd><?php echo $answer ?></dd>
+
+                    <?php endwhile; ?>
+
+                </dl>
+
+        <?php endif; ?> 
+  
+        </main><!-- #main -->
+
+    </div><!-- #primary -->
+
+
+    <?php get_sidebar(); ?>
+
+    </div><!-- #content -->
+
+<?php get_footer(); ?>
+
+<script>
+
+function openFirstPanel(){
+  $('.accordion > dt:first-child').next().addClass('active').slideDown();
+}
+
+(function($) {
+    
+  var allPanels = $('.accordion > dd').hide();
+  
+  openFirstPanel();
+    
+  $('.accordion > dt > a').click(function() {
+
+    
+
+      $this = $(this);
+      $target =  $this.parent().next();
+    
+    $target.prev().css( "background-color", "red" );
+
+      if($target.hasClass('active')){
+        $target.removeClass('active').slideUp(); 
+      }else{
+        allPanels.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+      }
+      
+    return false;
+  });
+
+})(jQuery);
+</script>
+
+
+
+
+
+
